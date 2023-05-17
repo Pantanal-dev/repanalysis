01a235815b150ef1c25d87fcb51a08b4db20cb79
Leandro Pereira <leojgpereira@gmail.com>
2023-05-17 19:17:37 -0400

    HTML e CSS da página inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
-  Home
+  <img src="@/assets/images/logo.png" alt="Rep Analysis Logo" class="logo">
+
+  <form class="search" method="get" action="/report">
+    <input type="text" class="search-field" placeholder="Qual empresa gostaria de analisar?" spellcheck="false" v-model="tweet">
+
+    <div class="search-button-wrapper">
+      <button type="submit" class="search-button" @click.prevent="">
+        <vue-feather type="search" class="search-icon"></vue-feather>
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
+import VueFeather from 'vue-feather';
 import "@/assets/styles/home.css";
 
 export default {
   name: 'HomeView',
+  components: {
+    VueFeather
+  }
 }
 </script>
