3a437d66df8bad3b93e3f6953b8c21cd8ff168d8
Leandro Pereira <leojgpereira@gmail.com>
2023-05-17 19:49:35 -0400

    HTML e CSS da página report

--- src/views/ReportView.vue
+++ src/views/ReportView.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="report">
-    Report: {{ $route.params.companyName }}
+  <nav class="header">
+    <div class="wrapper">
+      <a href="/">
+        <img src="../assets/images/logo.png" alt="Rep Analysis Logo" class="logo">
+      </a>
+    </div>
+  </nav>
+  <div class="content">
+    <div class="wrapper">
+      
+    </div>
   </div>
+  <footer class="footer">
+    <div class="wrapper">
+      <p>&copy; 2023 Rep Analysis</p>
+      <p> &lt; Pantanal.dev &gt; </p>
+    </div>
+  </footer>
 </template>
 
 <script>
