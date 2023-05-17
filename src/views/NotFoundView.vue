cf361d64f17730251f96b4a904f599f7e8ba91f1
Leandro Pereira <leojgpereira@gmail.com>
2023-05-17 18:38:29 -0400

    Estrutura básica das páginas

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="not-found">
+    Not Found
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView',
+}
+</script>
---
+++ src/views/ReportView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="report">
+    Report: {{ $route.params.companyName }}
+  </div>
+</template>
+
+<script>
+import "@/assets/styles/report.css"
+
+export default {
+  name: 'ReportView',
+}
+</script>
