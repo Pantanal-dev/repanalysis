b00501e8ff491698afd17de956828ded25b01331
Leandro Pereira <leojgpereira@gmail.com>
2023-05-19 17:57:49 -0400

    Conectando ao Colab

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <img src="@/assets/images/logo.png" alt="Rep Analysis Logo" class="logo">
 
-  <form class="search" method="get" action="/report">
+  <form class="search" method="get" action="/report/dell">
     <input type="text" class="search-field" placeholder="Qual empresa gostaria de analisar?" spellcheck="false" v-model="tweet">
 
     <div class="search-button-wrapper">
-      <button type="submit" class="search-button" @click.prevent="">
+      <button type="submit" class="search-button" onclick="">
         <vue-feather type="search" class="search-icon"></vue-feather>
       </button>
     </div>
